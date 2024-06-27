5c4858e09f3b4f2c1a9b2164ff3deba1ea6ee332
gutormolina <augustomolina300@gmail.com>
2024-06-27 19:23:21 -0300

    in progress

--- src/main.c
+++ src/main.c
@@ -2,7 +2,29 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+    if (argc != 3)
+    {
+        printf("Uso: %s <arquivo_de_entrada> <n_processadores>\n", argv[0]);
+
+        return 1;
+    }
+    
+    char *arquivo_entrada = argv[1];
+    int n_processadores = atoi(argv[2]);
+
+    FILE *fp = fopen(arquivo_entrada, "r");
+
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir arquivo\n");
+
+        return 1;
+    }
+
+    while (!EOF)
+    {
+        fread();
+    }
 
     return 0;
 }
